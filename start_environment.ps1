573aab1fc8e55657733658170cc5caa15d5db1f3
bewiss <wissmeier@googlemail.com>
2020-11-15 20:07:34 +0100

    powershell to start registration service and mongoDB

---
+++ start_environment.ps1
@@ -0,0 +1,23 @@
+﻿$ServiceName = 'MongoDB'
+$arrService = Get-Service -Name $ServiceName
+
+while ($arrService.Status -ne 'Running')
+{
+
+    Start-Service $ServiceName
+    write-host $arrService.status
+    write-host 'Service starting'
+    Start-Sleep -seconds 60
+    $arrService.Refresh()
+    if ($arrService.Status -eq 'Running')
+    {
+        Write-Host 'Service is now Running'
+    }
+
+}
+
+Write-host "start login service"
+d:
+cd D:\projekte\angular\login\node-mongo-signup-verification-api
+npm start
+pause
